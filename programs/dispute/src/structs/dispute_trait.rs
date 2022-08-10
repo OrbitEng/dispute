61f8d7e979a3325605b5534fc1538b1ed900b07a
indianeros58 <0xDEADBEAT@gmail.com>
2022-08-10 16:54:07 -0400

    change trait

--- programs/dispute/src/structs/dispute_trait.rs
+++ programs/dispute/src/structs/dispute_trait.rs
@@ -1,7 +1,9 @@
 use anchor_lang::prelude::*;
 
 
-pub trait OrbitDisputableTrait<'a, 'b, T, U>{
+pub trait OrbitDisputableTrait<'a, 'b, T, U>
+    where T: Accounts<'a>, U: Accounts<'b>
+{
     fn freeze(ctx: Context<T>) -> Result<()>;
     fn unfreeze(ctx: Context<U>) -> Result<()>;
 }
