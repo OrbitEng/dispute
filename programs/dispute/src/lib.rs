4e85164986306296ea80002a58fea561980f8dee
indianeros58 <0xDEADBEAT@gmail.com>
2022-08-14 00:21:56 -0400

    ?

--- programs/dispute/src/lib.rs
+++ programs/dispute/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("HPDHvMeJ2Vepn5jEmsWNvEJwgFhY3WoNFZapKxQxQhDK");
 
 pub mod structs;
 pub mod accessors;
