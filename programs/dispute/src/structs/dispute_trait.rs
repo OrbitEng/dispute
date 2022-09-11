8b9f0ddb57224c37b9391f89ae5b25743a216aaf
indianeros58 <0xDEADBEAT@gmail.com>
2022-09-11 01:31:41 -0400

    use discount

--- programs/dispute/src/structs/dispute_trait.rs
+++ programs/dispute/src/structs/dispute_trait.rs
@@ -5,6 +5,6 @@ pub trait OrbitDisputableTrait<'a, 'b, 'c, T, U, V>
     where T: Accounts<'a>, U: Accounts<'b>, V: Accounts<'c>
 {
     fn open_dispute(ctx: Context<T>, threshold: u8) -> Result<()>;
-    fn close_dispute_sol(ctx: Context<U>, use_discount: bool) -> Result<()>;
-    fn close_dispute_spl(ctx: Context<V>, use_discount: bool) -> Result<()>;
+    fn close_dispute_sol(ctx: Context<U>) -> Result<()>;
+    fn close_dispute_spl(ctx: Context<V>) -> Result<()>;
 }
